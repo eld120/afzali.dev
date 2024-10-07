3a7c0f07a077c2e60c58780176f0bacfa926786c
Seyamack Afzali <seyamack@gmail.com>
2024-10-06 22:25:03 -0500

    last component for now

---
+++ app/components/Services.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <section>
+            <div>
+                <h1>Building Lean Software Solutions</h1>
+                <p>Delivering Value </p>
+            </div>
+            <div><h2>Web Development</h2>
+                <p></p>
+                <ul>
+                    <li>Django</li>
+                    <li>NextJS/React</li>
+                    <li>Shopify</li>
+                    <li>WordPress</li>
+                    <li></li>
+                </ul>
+
+            </div>
+            <div><h2>Mobile Development</h2>
+                <p>
+
+                </p>
+
+
+            </div>
+            <div><h2>Hosting and Support</h2>
+                <p>
+
+                </p>
+
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Services
