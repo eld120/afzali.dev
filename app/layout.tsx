4de738f413f22397ea3a8fce686fb6bfab9c0523
Seyamack Afzali <seyamack@gmail.com>
2024-12-16 00:25:46 -0600

    linting is working now, plus added nav/footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
-import { Providers } from './providers'
+const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: 'Seyamack Afzali',
-  description: 'Offering custom software developers for specialty retailers',
-}
+  title: "Seyamack Afzali",
+  description: "Custom Software Development for specialty retailers",
+};
 
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <head>
+          <link rel="icon" href="/static/logos/terminal.svg" sizes="32x32" type="image/svg" />
+        </head>
+
+        <body className={inter.className}>
+          <header>
+            {/* navbar */}
+            <NavBar />
+            <hr className="mb-8" />
+          </header>
+          {children}
+          <hr className="my-8" />
+          <Footer />
+        </body>
       </Providers>
     </html>
-  )
+  );
 }
