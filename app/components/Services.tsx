f40ad812df4e85a9a1d0c1a4c61206eede452519
Seyamack Afzali <seyamack@gmail.com>
2024-12-16 00:28:44 -0600

    linting/formatting changes

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,40 +1,35 @@
-import React from 'react'
-
 function Services() {
-    return (
-        <section>
-            <div>
-                <h1>Building Lean Software Solutions</h1>
-                <p>Delivering Value </p>
-            </div>
-            <div><h2>Web Development</h2>
-                <p></p>
-                <ul>
-                    <li>Django</li>
-                    <li>NextJS/React</li>
-                    <li>Shopify</li>
-                    <li>WordPress</li>
-                    <li></li>
-                </ul>
-
-            </div>
-            <div><h2>Mobile Development</h2>
-                <p>
-
-                </p>
-
-
-            </div>
-            <div><h2>Hosting and Support</h2>
-                <p>
-
-                </p>
-
-
-            </div>
-
-        </section>
-    )
+  return (
+    <section>
+      <div>
+        <h1>Building Solutions</h1>
+        <p>Delivering Value </p>
+        <p>
+          My career started at the intersection specialty retail, manufacturing, and the emergence of 3rd party
+          ecommerce marketplaces{" "}
+        </p>
+      </div>
+      <div>
+        <h2>Web Development</h2>
+        <p></p>
+        <ul>
+          <li>Django</li>
+          <li>NextJS/React</li>
+          <li>Shopify</li>
+          <li>WordPress</li>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Mobile Development</h2>
+        <p></p>
+      </div>
+      <div>
+        <h2>Hosting and Support</h2>
+        <p></p>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
--- app/components/SocialBar.tsx
+++ app/components/SocialBar.tsx
@@ -1,32 +1,26 @@
-import Link from 'next/link'
-import Image from 'next/image'
-function SocialBar() {
-    return (
-        <div className='flex gap-6 justify-center'>
-            {/* email */}
-            < Link href={"mailto:devteam@origma.io"} >
-                <Image
-                    src={"/static/Antu_mail-folder-sent.png"}
-                    width={36}
-                    height={36}
-                    alt='Email Me Logo'
-                />
-            </ Link>
-            {/* linkedin */}
-            < Link href={"https://www.linkedin.com/in/seyamack-a-912009202/"} >
-                <Image
-                    src="/static/LinkedIn_icon.svg" alt='LinkedIn Logo' width={36} height={36}
-                />
-            </ Link>
-            {   /* github */}
-            <Link href={"https://github.com/eld120"} >
-                <Image
-                    src="/static/github-mark-white.png" alt='Github Logo' width={36} height={36}
-                />
-            </Link >
-        </div>
+import Link from "next/link";
+import Image from "next/image";
+import LinkedIn from "/public/static/LinkedIn_icon.svg";
+import MailIcon from "/public/static/Antu_mail-folder-sent.png";
+import Github from "/public/static/github-mark-white.png";
 
-    )
+function SocialBar() {
+  return (
+    <div className="flex gap-6 justify-center">
+      {/* email */}
+      <Link href={"mailto:seyamack@origma.io"} className="hover:-translate-y-2">
+        <Image src={MailIcon} width={36} height={36} alt="Email Me Logo" />
+      </Link>
+      {/* linkedin */}
+      <Link href={"https://www.linkedin.com/in/seyamack-a-912009202/"} className="hover:-translate-y-2">
+        <Image src={LinkedIn} alt="LinkedIn Logo" width={36} height={36} />
+      </Link>
+      {/* github */}
+      <Link href={"https://github.com/eld120"} className="hover:-translate-y-2">
+        <Image src={Github} alt="Github Logo" width={36} height={36} />
+      </Link>
+    </div>
+  );
 }
 
-export default SocialBar
+export default SocialBar;
