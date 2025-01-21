a53c0779fd8e4cb3ea6426466a090e772e3f21ee
Seyamack Afzali <seyamack@gmail.com>
2025-01-21 01:31:17 -0600

    no client component needed

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-"use client";
-
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 export default function Home() {
   return (
-    <>
+    <div>
       {/* hero section */}
       <Hero />
       <Skills />
       <Portfolio />
-    </>
+    </div>
   );
 }
