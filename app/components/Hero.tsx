8cdee9b0651e03067a1ca8bc8ad83a6101560a9c
Seyamack Afzali <seyamack@gmail.com>
2025-04-05 02:26:20 -0500

    changed copy and updated contact button to mailto

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,10 +10,9 @@ function Hero() {
         <h1 className="text-2xl md:text-4xl font-bold mb-6">Hi I&apos;m Seyamack</h1>
         <div className="dark:text-gray-300  mb-4 font-light">
           <p className="mb-2">I&apos;m a software engineer based in the Minneapolis-St Paul metro</p>
-          <p>I handle the development and maintenance of your website or app from start to finish</p>
         </div>
         {/* <p className='text-lg mb-4'>I have over a decade of experience working with specialty retailers and manufacturers with a focus on the outdoor industry</p> */}
-        <Link href={"/contact"}>
+        <Link href={"mailto:seyamack@origma.io"}>
           <Button colorScheme="yellow" className="my-4">
             <span className="font-light">Let&apos;s&nbsp;</span> Work Together
           </Button>
